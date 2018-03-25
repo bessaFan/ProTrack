8da7f2f293523585edb5c4525f051414948a1081
bessafan <bessafan01@gmail.com>
2018-03-25 16:22:42 -0400

    default page

---
+++ app/src/main/java/com/example/protrack_bessa/default_page.java
@@ -0,0 +1,13 @@
+package com.example.protrack_bessa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class default_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default_page);
+    }
+}
